b6c418deb73bd407ecf85d00dbdcf2d6f02a1ce2
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-22 13:01:41 -0700

    add POST form

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,30 @@
-const exec = require('child_process').exec;
+const queryString = require('querystring');
 
-function start(res) {
+function start(res, postData) {
   console.log('Request handler \'start\' was called.');
 
-  exec('find /',
-    { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(stdout);
+  const body =  '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(body);
     res.end();
-  });
 }
 
-function upload(res) {
+function upload(res, postData) {
   console.log('Request handler \'upload\' was called.');
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(stdout);
+  res.write(`You've sent the text: ${queryString.parse(postData).text}`);
   res.end();
 }
 
