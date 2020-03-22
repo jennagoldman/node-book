65da3682e55c9d8bcabc752c204dc58eab7fa0a1
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-22 11:45:47 -0700

    sunday morning commit

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
-function onRequest(req, res) {
+function start() {
+  function onRequest(req, res) {
     console.log('Request received');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World');
     res.end();
-};
+  };
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+}
+exports.start = start;
